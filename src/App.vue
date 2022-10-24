8da37c4be80c81c900985456e02dd59a5dc3c8f9
Jose Chirivella <jrchg1403@gmail.com>
2022-10-24 15:19:10 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.svg" />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  img {
+    width: 10%;
+  }
+}
+</style>
